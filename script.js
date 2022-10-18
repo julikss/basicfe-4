e9d16224f7deabca9d20e28579f74962c616b295
DESKTOP-TQL0JT6\juliks <julie.lehenka@gmail.com>
2022-10-19 00:06:49 +0300

    Add task2

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+//task 1 (56 mod 10 + 1 = 6 + 1 => 7)
 const task1 = {
     firstClick: document.getElementById('firstClick'),
     secondClick: document.querySelector('#secondClick')
@@ -25,3 +26,39 @@ task1.secondClick.addEventListener('click', () => {
         task1.secondClick.classList.remove('first-active');
     }
 });
+
+
+//task2
+const imgPlace = document.getElementById('img-placeholder');
+
+const addImg = () => {
+    const copy = document.getElementById('copyImg');
+    if (!copy) {
+        let img = document.createElement('img');
+            img.id = 'copyImg';
+            img.src = './img/kyiv.jpg';
+            img.alt = 'Kyiv';
+            img.width = '500';
+        imgPlace.append(img);
+    }
+}
+
+let zoomValue = 1;
+const zoomIn = () => {
+    const copy = document.getElementById('copyImg');
+    zoomValue+=0.1;
+    if (!copy || zoomValue < 1) return;
+    copy.style.transform = `scale(${zoomValue})`;
+}
+
+const zoomOut = () => {
+    const copy = document.getElementById('copyImg');
+    zoomValue-=0.1;
+    if (!copy || zoomValue < 1) return;
+    copy.style.transform = `scale(${zoomValue})`;
+}
+
+const removeImg = () => {
+    const copy = document.getElementById('copyImg');
+    if (copy) copy.remove();
+}
